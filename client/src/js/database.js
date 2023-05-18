9f70c2e9dc143aa840d6b6ade13f2b366bb29321
sswiger <sswiger@forhims.com>
2023-05-17 20:27:24 -0700

    finished app

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,28 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  try {
+  const jate = await openDB("jate", 1);
+  const tx = jate.transaction("jate", "readwrite");
+  const store = tx.objectStore("jate");
+  const request = store.put({ id: 1, value: content });
+  const result = await request;
+} catch (err) {
+  console.error('putDb not implemented', err);
+}
+};
+
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const jate = await openDB("jate", 1);
+  const tx = jate.transaction("jate", "readonly");
+  const store = tx.objectStore("jate");
+  const request = store.get(1);
+  const result = await request;
+  console.log("This is request.value", result?.value);
+  return result?.value;
+}
 
 initdb();
